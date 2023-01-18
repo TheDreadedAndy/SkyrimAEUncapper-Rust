4b6f5cd5872a3feb9ae65470b947329d83fcfc1a
TheDreadedAndy <andyespaulding@gmail.com>
2023-01-17 22:02:07 -0500

    Set up bindgen for skse

---
+++ ctypes/Readme.md
@@ -0,0 +1,6 @@
+# C-Types
+
+This file was originally written for a different project of mine which
+had *a ton* of C-FFI. It's generally useful, so I pasted it in here.
+
+But I probably won't use the extern method/invoker stuff for the uncapper.
--- skse64/build.rs
+++ skse64/build.rs
@@ -18,6 +18,12 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header(WRAPPER_HEADER)
         .use_core()
+        .clang_arg("-xc++")
+        .clang_arg("-std=c++17")
+        .clang_arg("-I../skse64_src/common/")
+        .clang_arg("-I../skse64_src/skse64/")
+        .clang_arg("-I../skse64_src/skse64/skse64/")
+        .clang_arg("-I../skse64_src/skse64/skse64_common/")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate().unwrap();
 
