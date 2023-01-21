43397b6b14f294d6ef228461f6a14db233ac0ea3
TheDreadedAndy <andyespaulding@gmail.com>
2023-01-21 14:18:21 -0500

    Fixed resource information

--- SkyrimUncapper/build.rs
+++ SkyrimUncapper/build.rs
@@ -5,46 +5,21 @@
 //! @bug No known bugs.
 //!
 
-use std::process::Command;
-use std::path::PathBuf;
-
-const RESOURCE_FILE: &str = "SkyrimUncapper.rc";
-const RESOURCE_HEADER: &str = "resource.h";
-const RC_BIN: &str = "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.22000.0\\x64\\rc.exe";
-const RC_INC: &str = "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.22000.0\\um";
-const RC_INC_SHARED: &str = "C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.22000.0\\shared";
+const RC_AUTHOR: &str = "Kasplat";
+const RC_NAME: &str = "Skyrim Uncapper AE";
+const RC_VERSION: &str = "2.0.0.0";
+const RC_FILE: &str = "SkyrimUncapper.dll";
 
 fn main() {
-    // Mark our RC as a dependency.
-    println!("cargo:rerun-if-changed={}", RESOURCE_FILE);
-    println!("cargo:rerun-if-changed={}", RESOURCE_HEADER);
-
-    // Build the RC file.
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let output_file = out_dir.join("resource.res");
-    Command::new(RC_BIN).args(&[
-        "/D \"_UNICODE\"",
-        "/D \"UNICODE\"",
-        "/l 0x0409",
-        "/nologo",
-        "/I", RC_INC,
-        "/I", RC_INC_SHARED,
-        "/r",
-        "/fo",
-        output_file.as_os_str().to_str().unwrap(),
-        RESOURCE_FILE
-    ]).status().unwrap();
-
-    // Create a static library out of the resource file.
-    let ar_bin = PathBuf::from(std::env::var("LIBCLANG_PATH").unwrap()).join("llvm-ar.exe");
-    let resource_lib = out_dir.join("resource.lib");
-    Command::new(ar_bin).args(&[
-        "rc",
-        resource_lib.as_os_str().to_str().unwrap(),
-        output_file.as_os_str().to_str().unwrap()
-    ]).status().unwrap();
-
-    // Link in the resource file.
-    println!("cargo:rustc-link-search={}", out_dir.as_os_str().to_str().unwrap());
-    println!("cargo:rustc-link-lib=static=resource");
+    // Embed resource information.
+    let mut res = winres::WindowsResource::new();
+    res.set("CompanyName", RC_AUTHOR);
+    res.set("FileDescription", RC_NAME);
+    res.set("FileVersion", RC_VERSION);
+    res.set("InternalName", RC_FILE);
+    res.set("LegalCopyright", "Copyright (C) 2023");
+    res.set("OriginalFilename", RC_FILE);
+    res.set("ProductName", RC_NAME);
+    res.set("ProductVersion", RC_VERSION);
+    res.compile();
 }
