3321d92bfb4d6df8bca2d982c22b2ff5d7174576
Andrew Spaulding <andyespaulding@gmail.com>
2023-03-18 20:12:31 -0700

    Improved the way huffman trees are stored in the compressed lz file

--- lib/deflate/src/huff.rs
+++ lib/deflate/src/huff.rs
@@ -5,6 +5,7 @@
 //! @bug No known bugs.
 //!
 
+use std::cmp::Ordering;
 use std::collections::VecDeque;
 
 use crate::bits::*;
@@ -15,13 +16,11 @@ const NUM_PHRASES: usize = u8::MAX as usize + 1;
 
 /// A leaf in the huffman tree.
 struct HTreeLeaf {
-    weight: usize,
     phrase: u8
 }
 
 /// A node in a huffman encoding tree.
 struct HTreeNode {
-    weight: usize,
     left: HTreeData,
     right: HTreeData
 }
@@ -36,46 +35,6 @@ enum HTreeData {
 struct HTree(HTreeData);
 
 impl HTreeData {
-    /// Creates a data node from a stream of bits. See HTree::from_stream() for details.
-    fn from_stream(
-        bits: &mut BitStream<'_>
-    ) -> Self {
-        match bits.next().unwrap() {
-            Bit::One => {
-                let left = Self::from_stream(bits);
-                let right = Self::from_stream(bits);
-                Self::Link(Box::new(HTreeNode { weight: 0, left, right }))
-            },
-            Bit::Zero => {
-                let mut phrase = 0;
-                for i in 0..u8::BITS {
-                    phrase |= (bits.next().unwrap() as u8) << i;
-                }
-                Self::Leaf(HTreeLeaf { weight: 0, phrase })
-            }
-        }
-    }
-
-    /// Serializes the data into a stream of bits.
-    fn into_bits(
-        &self,
-        bits: &mut BitVec
-    ) {
-        match self {
-            Self::Link(node) => {
-                bits.push(Bit::One);
-                node.left.into_bits(bits);
-                node.right.into_bits(bits);
-            },
-            Self::Leaf(leaf) => {
-                bits.push(Bit::Zero);
-                for i in 0..u8::BITS {
-                    bits.push(if (leaf.phrase >> i) & 1 == 1 { Bit::One } else { Bit::Zero });
-                }
-            }
-        }
-    }
-
     /// Converts this data item and its children into an encoding table.
     fn into_encode_table(
         &self,
@@ -98,26 +57,57 @@ impl HTreeData {
         }
     }
 
-    /// Gets the weight of the underlying data.
-    fn weight(
-        &self
-    ) -> usize {
+    /// Converts this data item and its children into a phrase length table.
+    fn into_length_table(
+        &self,
+        depth: usize,
+        table: &mut [u8]
+    ) {
         match self {
-            Self::Link(node) => node.weight,
-            Self::Leaf(leaf) => leaf.weight
+            Self::Link(node) => {
+                node.left.into_length_table(depth + 1, table);
+                node.right.into_length_table(depth + 1, table);
+            },
+            Self::Leaf(node) => {
+                assert!(depth > 0);
+                table[node.phrase as usize] = depth.try_into().unwrap();
+            }
+        }
+    }
+
+    /// Creates a data element from a pair table and depth.
+    fn from_pair_table(
+        depth: usize,
+        index: &mut usize,
+        table: &[(u8, u8)]
+    ) -> Self {
+        if table[*index].1 == depth.try_into().unwrap() {
+            let ret = Self::Leaf(HTreeLeaf { phrase: table[*index].0 });
+            *index += 1;
+            return ret;
+        } else {
+            let left = Self::from_pair_table(depth + 1, index, table);
+            let right = Self::from_pair_table(depth + 1, index, table);
+            return Self::Link(Box::new(HTreeNode { left, right }));
         }
     }
 }
 
 impl HTree {
+    /// The number of used in the max code length prefix of a huffman tree serialization.
+    const LEN_PREFIX_BITS: usize = 4;
+
     /// Creates a huffman tree for the phrases in the given data.
     fn new(
         data: &[u8]
     ) -> Self {
         // Gets a minimum weighted element from the two queues.
-        let qmin = |l: &mut VecDeque<HTreeData>, r: &mut VecDeque<HTreeData>| -> HTreeData {
+        let qmin = |
+            l: &mut VecDeque<(usize, HTreeData)>,
+            r: &mut VecDeque<(usize, HTreeData)>
+        | -> (usize, HTreeData) {
             if l.front().is_some() && r.front().is_some() {
-                if l.front().unwrap().weight() < r.front().unwrap().weight() {
+                if l.front().unwrap().0 < r.front().unwrap().0 {
                     l.pop_front().unwrap()
                 } else {
                     r.pop_front().unwrap()
@@ -134,37 +124,15 @@ impl HTree {
         while base_q.len() + work_q.len() > 1 {
             let left = qmin(&mut base_q, &mut work_q);
             let right = qmin(&mut base_q, &mut work_q);
-            work_q.push_back(HTreeData::Link(Box::new(HTreeNode {
-                weight: left.weight() + right.weight(),
-                left,
-                right
-            })));
+            work_q.push_back((left.0 + right.0, HTreeData::Link(Box::new(HTreeNode {
+                left: left.1,
+                right: right.1
+            }))));
         }
 
-        Self(qmin(&mut base_q, &mut work_q))
-    }
-
-    ///
-    /// Creates an HTree from a bit stream.
-    ///
-    /// HTrees are encoded starting from the root, with a leaf being prefixed by a zero bit and
-    /// a node being prefixed by a 1 bit. A leaf will contain its phrase, while a node will be
-    /// followed by its left child (and all of its left childs decendents) and then its right
-    /// child.
-    ///
-    fn from_stream(
-        bits: &mut BitStream<'_>
-    ) -> Self {
-        Self(HTreeData::from_stream(bits))
-    }
-
-    /// Serializes this instance into a bit vector.
-    fn into_bits(
-        &self
-    ) -> BitVec {
-        let mut ret = BitVec::new();
-        self.0.into_bits(&mut ret);
-        return ret;
+        // Make the tree into a canonical encoding.
+        let tree = Self(qmin(&mut base_q, &mut work_q).1);
+        Self::from_length_table(tree.into_length_table())
     }
 
     /// Consumes data from a bit stream, decoding it into a phrase.
@@ -200,10 +168,88 @@ impl HTree {
         return ret;
     }
 
+    /// Creates a encoding length table for this tree.
+    fn into_length_table(
+        &self
+    ) -> [u8; NUM_PHRASES] {
+        let mut ret = [0; NUM_PHRASES];
+        self.0.into_length_table(0, &mut ret);
+        return ret;
+    }
+
+    /// Creates a new huffman tree from a length table.
+    fn from_length_table(
+        table: [u8; NUM_PHRASES]
+    ) -> Self {
+        let mut groups = [(0, 0); NUM_PHRASES];
+        for i in 0..NUM_PHRASES { groups[i] = (i as u8, table[i]); }
+        groups.sort_by(|lhs, rhs| {
+            let len_cmp = lhs.1.cmp(&rhs.1);
+            if let Ordering::Equal = len_cmp {
+                lhs.0.cmp(&rhs.0)
+            } else {
+                len_cmp
+            }
+        });
+
+        let mut i = 0;
+        while groups[i].1 == 0 { i += 1; }
+        let mut index = 0;
+        Self(HTreeData::from_pair_table(0, &mut index, groups.split_at(i).1))
+    }
+
+    /// Serializes a length table into a bit vector.
+    fn serialize_length_table(
+        table: [u8; NUM_PHRASES],
+        vec: &mut BitVec
+    ) {
+        let width = |n: u8| { if n == 0 { 1u8 } else { (n.ilog2() + 1) as u8 } };
+
+        // Determine how many bits are needed to represent each length.
+        let mut max_len: u8 = 0;
+        for b in table.iter() {
+            max_len = std::cmp::max(max_len, width(*b));
+        }
+        assert!(max_len as u32 <= u8::BITS);
+
+        for i in 0..Self::LEN_PREFIX_BITS {
+            vec.push(if (max_len & (1 << i)) == 0 { Bit::Zero } else { Bit::One });
+        }
+
+        for b in table.iter() {
+            for i in 0..max_len {
+                vec.push(if (b & (1 << i)) == 0 { Bit::Zero } else { Bit::One });
+            }
+        }
+    }
+
+    /// Deserializes a length table from a bit stream.
+    fn deserialize_length_table(
+        stream: &mut BitStream<'_>
+    ) -> [u8; NUM_PHRASES] {
+        // Get bit width of each length.
+        let mut max_len: u8 = 0;
+        for i in 0..Self::LEN_PREFIX_BITS {
+            if stream.next().unwrap() == Bit::One {
+                max_len |= 1 << i;
+            }
+        }
+
+        let mut ret = [0; NUM_PHRASES];
+        for i in 0..NUM_PHRASES {
+            for j in 0..max_len {
+                if stream.next().unwrap() == Bit::One {
+                    ret[i] |= 1 << j;
+                }
+            }
+        }
+        return ret;
+    }
+
     /// Creates a queue of huffman nodes from a base data stream.
     fn create_base_queue(
         data: &[u8]
-    ) -> VecDeque<HTreeData> {
+    ) -> VecDeque<(usize, HTreeData)> {
         assert!(data.len() > 0);
         let mut freq = [0; NUM_PHRASES];
         for b in data.iter() {
@@ -213,12 +259,12 @@ impl HTree {
         let mut base_q = Vec::new();
         for (b, f) in freq.iter().enumerate() {
             if *f > 0 {
-                base_q.push(HTreeData::Leaf(HTreeLeaf { weight: *f, phrase: b as u8 }));
+                base_q.push((*f, HTreeData::Leaf(HTreeLeaf { phrase: b as u8 })));
             }
         }
 
         assert!(base_q.len() > 0);
-        base_q.sort_by(|lhs, rhs| lhs.weight().cmp(&rhs.weight()));
+        base_q.sort_by(|lhs, rhs| lhs.0.cmp(&rhs.0));
         return VecDeque::from(base_q);
     }
 }
@@ -232,7 +278,7 @@ pub fn compress(
 
     let mut ret = BitVec::from_vec(ret);
     let tree = HTree::new(data);
-    ret.append(&tree.into_bits());
+    HTree::serialize_length_table(tree.into_length_table(), &mut ret);
 
     let table = tree.into_encode_table();
     for b in data.iter() {
@@ -250,7 +296,7 @@ pub fn decompress(
     let size = size as usize;
     let mut stream = BitStream::from_slice(&data[n..]);
 
-    let tree = HTree::from_stream(&mut stream);
+    let tree = HTree::from_length_table(HTree::deserialize_length_table(&mut stream));
     let mut ret = Vec::new();
     for _ in 0..size {
         ret.push(tree.decode(&mut stream));
